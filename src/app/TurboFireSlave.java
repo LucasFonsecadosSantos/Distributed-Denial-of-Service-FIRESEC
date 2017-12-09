e6930f029724a381580ed68802ec8e795793c712
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2017-12-09 22:54:11 +0000

    Is there some threads bugs

--- src/app/TurboFireSlave.java
+++ src/app/TurboFireSlave.java
@@ -1,8 +1,26 @@
 package app;
 
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.net.Socket;
 import view.GUI;
 import utilies.Interpreter;
 
 public class TurboFireSlave {
+
+    private GUI gui;
+
+    public TurboFireSlave() {
+        try {
+            Socket socket = new Socket("127.0.0.1", 2525);
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            output.writeObject("online");
+            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+            String s = (String)in.readObject();
+            System.out.println(s);
+        }catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 
 }
--- src/utilies/ClientThread.java
+++ src/utilies/ClientThread.java
@@ -8,12 +8,18 @@ public class ClientThread extends Thread {
 
     public ClientThread(Socket client) {
         this.client = client;
+        System.out.println("crioou");
     }
 
     @Override
     public void run() {
         try {
-
+            /*
+            ObjectInputStream input = new ObjectInputStream(this.client.getInputStream());
+            String message = (String)input.readObject();
+            if(message.equals("online")) {
+                ObjectOutputStream output = new ObjectOutputStream(this.client.getOutputStream("OK"));
+            }*/
         }catch (Exception e) {
             System.out.println(e.toString());
         }
