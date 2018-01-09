7a056fa49c38a2b9835f049db60c2efadcf1ab49
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2018-01-09 16:57:25 -0500

    TransmissionControlProtocolInterface class Documentation

--- src/protocols/interfaces/TransmissionControlProtocolInterface.java
+++ src/protocols/interfaces/TransmissionControlProtocolInterface.java
@@ -1,3 +1,19 @@
+/**
+ * FIRESEC TURBO FIRE
+ * A stress network analysis tool.
+ * ------------------------------------
+ * Written by Lucas Fonseca dos Santos.
+ * Copyleft 2018 - Rights not reserved.
+ * ------------------------------------
+ * 
+ * Pratical project for discipline Distributed
+ * Systems of Federal Univeristy of Lavras - MG,
+ * Brazil.
+ * 
+ * CONTACT:
+ * lucas@lcfcompany.com.br
+ * github.com/LucasFonsecaDosSantos
+ */
 package protocols.interfaces;
 
 import utilies.AttackPattern;
@@ -10,18 +26,45 @@ import java.net.UnknownHostException;
 
 import app.TurboFireSlave;
 
+/**
+ * @author Lucas Fonseca dos Santos
+ * @author Otavio Andrade
+ * @version 1.0
+ * 
+ * This abstract class describes the Transmission Control
+ * Protocol interface, specific of Protocol (so it extends protocol
+ * abstract class). It contains the algorithm for a TCP transmission
+ * in network by JVM and other settings about DDOS attack operation.
+ */
 public abstract class TransmissionControlProtocolInterface extends Protocol {
 
+    /**
+     * Socket connection to server.
+     */
     private Socket socket;
 
+    /**
+     * Transmission control protocol interface constuctor. It
+     * receives a attack pattern object, the master server address
+     * and the master server port and send these parameters to super class,
+     * Protocol abstract class in this case.
+     * 
+     * @param attackPatter The attack pattern object.
+     * @param masterAddress The master server IP address.
+     * @param masterPort THe master server port value.
+     */
     public TransmissionControlProtocolInterface(AttackPattern attackPattern, String masterAddress, int masterPort) {
         super(attackPattern, masterAddress, masterPort);
     }
 
+    /**
+     * How the super class Protocol implements the Runnable interface,
+     * this class implements the run method for thread task.
+     * In this method, all socket connection will be initialized and
+     * the message will be transmited.
+     */
     @Override
     public void run() {
-        
-        
         //SE DER RUIM AQUI, TEM QUE COLOCAR CONDICAO DE PARADA STOP THREAD
         //while(!Thread.currentThread().isInterrupted() && (this.socket.isConnected() && !this.socket.isClosed())) {
         while(true) {
@@ -38,6 +81,11 @@ public abstract class TransmissionControlProtocolInterface extends Protocol {
         }
     }
 
+    /**
+     * This method is responsible for creating a new
+     * socket connection and setting other settings as
+     * socket time out and socket inet address.
+     */
     public void createConnection() {
         setAddress(new InetSocketAddress(getAttackPattern().getIP(), getAttackPattern().getPort()));
         this.socket = new Socket();
@@ -52,6 +100,10 @@ public abstract class TransmissionControlProtocolInterface extends Protocol {
         }
     }
 
+    /**
+     * This method does the connection with target
+     * by socket.
+     */
     public void connect() {
         try {
             if(this.socket != null) {
@@ -68,6 +120,10 @@ public abstract class TransmissionControlProtocolInterface extends Protocol {
         }
     }
 
+    /**
+     * This method is responsible for closing the
+     * socket connection with target.
+     */
     public void closeConnection() {
         try {
             if(this.socket != null && !this.socket.isClosed() && this.socket.isBound()) {
@@ -80,10 +136,20 @@ public abstract class TransmissionControlProtocolInterface extends Protocol {
         }
     }
 
+    /**
+     * The socket state attribute modifier method.
+     * 
+     * @param socket A socket object.
+     */
     public void setSocket(Socket socket) {
         this.socket = socket;
     }
 
+    /**
+     * The socket state attribute accessor method.
+     * 
+     * @return Socket A socket object.
+     */
     public Socket getSocket() {
         return this.socket;
     }
